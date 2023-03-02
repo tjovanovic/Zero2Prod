5dd45dbb1e5d94914b28278782387176ba07e5e6
Tomislav Jovanovic <tjovanovic994@gmail.com>
2023-03-02 22:26:13 +0100

    chore: configurable port

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,14 +11,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
